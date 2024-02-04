a9cf106dfebf1538e633bc9b00b1112c05112a47
Phoebe Chen <phoebehch@gmail.com>
2024-02-04 19:26:40 +0000

    Modify rolling_agg_n_periods Macro

--- macros/rolling_agg_n_periods.sql
+++ macros/rolling_agg_n_periods.sql
@@ -1,7 +1,9 @@
-{% macro rolling_agg_n_periods(agg='AVG', column_name='event_id', partition_by='user_id', order_by='created_at', n=7) %}
+{%- macro rolling_agg_n_periods(agg, column_name, partition_by, order_by='created_at', n=7) -%}
+
     {{ agg }}( {{ column_name }} ) OVER (
                 PARTITION BY {{ partition_by }}
                 ORDER BY {{ order_by }}
-                ROWS BETWEEN {{ n }} PRECEDING AND CURRENT ROW
+                ROWS BETWEEN {{ n-1 }} PRECEDING AND CURRENT ROW
             ) AS {{ agg |lower }}_{{ n }}_periods_{{ column_name }}
-{% endmacro %}
+
+{%- endmacro %}
