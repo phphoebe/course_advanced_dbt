1bf67cc651e5906446257c9e0a7b2f8a9bd18a9d
Phoebe Chen <phoebehch@gmail.com>
2024-02-04 19:33:13 +0000

    Add one whitespace

--- macros/date_trunc.sql
+++ macros/date_trunc.sql
@@ -1,5 +1,5 @@
 {%- macro date_trunc(date_column, date_part='month') -%}
 
-    DATE_TRUNC('{{ date_part }}', DATE({{ date_column }})) as date_{{ date_part}}
+    DATE_TRUNC('{{ date_part }}', DATE({{ date_column }})) as date_{{ date_part }}
 
 {%- endmacro -%}
