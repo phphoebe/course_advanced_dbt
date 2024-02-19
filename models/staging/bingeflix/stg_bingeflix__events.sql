22def0c5939b624c2d88158c09a16d2461f4c59e
Phoebe Chen <phoebe.chen@koho.ca>
2024-02-19 21:57:25 +0000

    Convert stg_bingeflix__events to incremental

--- models/staging/bingeflix/stg_bingeflix__events.sql
+++ models/staging/bingeflix/stg_bingeflix__events.sql
@@ -1,6 +1,20 @@
+{{
+    config(
+        materialized='incremental',
+        unique_key='event_id'
+    )
+}}
+
+
 WITH source AS (
 
     SELECT * FROM {{ source('bingeflix', 'events') }}
+
+    {% if is_incremental() %}
+
+    WHERE created_at > (SELECT DATEADD('day', -3, MAX(created_at))) FROM {{ this }}
+
+    {% endif %}
 
 ),
 
